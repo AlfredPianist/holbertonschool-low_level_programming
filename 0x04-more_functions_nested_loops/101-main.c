ad6eb7145a090a257fc007439294f46fdfb1f550
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-02-11 18:13:48 -0500

    Added task 12 from 0x04

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints the character "#" in a squared triangle shape
+ *              of n lines, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(0);
+	_putchar('\n');
+	print_number(3);
+	_putchar('\n');
+	print_number(-1);
+	_putchar('\n');
+	print_number(10);
+	_putchar('\n');
+	print_number(-99);
+	_putchar('\n');
+	print_number(-1024);
+	_putchar('\n');
+	print_number(-4096);
+	_putchar('\n');
+	print_number(123456789);
+	_putchar('\n');
+	print_number(2147483647);
+	_putchar('\n');
+	print_number(-2147483648);
+	_putchar('\n');
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
