f4dc2161b105c54f14d0d3d659da88884e4189fa
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-02-18 14:00:14 -0500

    Modified holberton.h, added prototype of _putchar function

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * *_strcat - concatenates two strings overwriting the NUL character
  *            from the first string.
  * @dest: the destination string.
