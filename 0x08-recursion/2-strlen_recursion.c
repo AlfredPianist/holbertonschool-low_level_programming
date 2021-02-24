d596df1709615fc52519ab49ece129eaccfbaa89
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-02-24 21:04:51 +0000

    Fixed task 2 from 0x08, betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,8 @@
 /**
  * _strlen_recursion - returns the length of a string in a recursive manner.
  * @s: the string to be print.
+ *
+ * Return: the length of the string.
  */
 int _strlen_recursion(char *s)
 {
