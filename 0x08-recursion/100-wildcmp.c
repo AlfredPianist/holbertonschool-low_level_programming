7647b481930e364e142e1dbcca5dfbca9221d676
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-02-25 18:36:51 +0000

    Fixed 100 from 0x08, betty

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -4,6 +4,8 @@
  * wildcmp - compares two strings. The second one may have the * wildcard.
  * @s1: the source string.
  * @s2: the string to be compared to.
+ *
+ * Return: 1 if two strings are equal even with *, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -16,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		if (*s1 != *(s2 + 1))
-			return (wildcmp(s1 + 1, s2)); 
+			return (wildcmp(s1 + 1, s2));
 		if (*(s1 + 1) == *(s2 + 1))
 			return (wildcmp(s1 + 1, s2 + 1));
 		if (wildcmp(s1 + 1, s2) == 1)
