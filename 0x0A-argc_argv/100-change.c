57cee6b23e49671a923211385bd5d1f99c9c057b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-02-27 01:54:48 +0000

    Added task 100 from 0x0A

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * main - make change with infinite amounts of 25, 10, 5, 2 and 1.
+ * @argc: The number of arguments.
+ * @argv: The argument vector.
+ *
+ * Description: makes change given an infinite ammount of coins of 25, 10, 5, 2
+ *              and 1. Implemented with a greedy algorithm.
+ * Return: 1 if number of arguments isn't 2 (program name and change to make),
+ *         0 otherwise.
+ */
+int main(int argc, char *argv[])
+{
+	int coins[] = {25, 10, 5, 2, 1};
+	int cents, index, remainder;
+
+	cents = index = remainder = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	remainder = atoi(argv[1]);
+	if (remainder <= 0)
+		printf("0\n");
+	else
+	{
+		while (remainder != 0)
+		{
+			if (remainder / coins[index] > 0)
+			{
+				cents += (remainder / coins[index]);
+				remainder -= (remainder / coins[index]) * coins[index];
+			}
+			else
+				index++;
+		}
+		printf("%d", cents);
+	}
+
+	return (0);
+}
