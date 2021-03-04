8157126f18b713b71e69705b8bf649e7a5754b75
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-04 17:33:27 +0000

    Fixed 100 from 0x0C, return ptr after malloc if earlier NULL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,17 +20,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
-	}
-
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
+		return (ptr);
 	}
 
 	p_realloc = malloc(new_size);
