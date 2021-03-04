3febc13b7ee12098214249fb1f87acadccb87daa
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-04 17:23:01 +0000

    Fixed 2 from 0x0C, don't calculate sizeof(size)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p_alloc = malloc(sizeof(size) * nmemb);
+	p_alloc = malloc(size * nmemb);
 	if (p_alloc == NULL)
 		return (NULL);
 
