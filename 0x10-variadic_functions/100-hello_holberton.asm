af2ad466f64669cc110aed6d0449e9f4a54fc69b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-11 23:34:10 +0000

    0x10: Fixed task 100 - comma

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,15 @@
 global main
 
 section .data
-	hello:	db 	"Hello Holberton", 10 	; Allocates memory for "Hello Holberton\n"
-	len:	equ	$-hello			; Defines the length of "Hello Holberton\n"
+	hello:	db 	"Hello, Holberton", 10 	; Allocates memory for "Hello, Holberton\n"
+	len:	equ	$-hello			; Defines the length of "Hello, Holberton\n"
 
 section .text
 main:
 	mov 	rax, 	1	; write (
 	mov 	rdi, 	1	; 	1 (to stdout),
-	mov 	rsi, 	hello	;	"Hello Holberton\n",
-	mov 	rdx, 	len	;	sizeof("Hello Holberton\n")
+	mov 	rsi, 	hello	;	"Hello, Holberton\n",
+	mov 	rdx, 	len	;	sizeof("Hello, Holberton\n")
 	syscall			; );
 	
    	mov	rax,	60	; exit (
