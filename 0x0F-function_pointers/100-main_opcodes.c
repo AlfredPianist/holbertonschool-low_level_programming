c536658e709284b7f84f752baf4afa1c58d7c13f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-11 02:19:11 +0000

    Added 100 from 0x0F.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the main function's instruction opcodes.
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	int (*p_address)(int, char **);
+	int num_bytes, counter;
+
+	num_bytes = counter = 0;
+
+	/* Exits if there is anything else than 2 arguments */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	/* Exits if number of bytes is negative */
+	if (argv[1][0] == '-')
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	/*
+	 * To print opcodes, the function pointer p_address points to the
+	 * main function.
+	 */
+	p_address = main;
+
+	/*
+	 * Because a function pointer points to actual instructions,
+	 * iterates through each byte from the pointer's address and casts it
+	 * to be an unsigned char * value for it to be printed then as hex (2 bytes).
+	 */
+	num_bytes = atoi(argv[1]);
+	for (counter = 0; counter < num_bytes; counter++)
+		printf("%.2x ", *(((unsigned char *) p_address) + counter));
+
+	printf("\n");
+
+	return (0);
+}
