89bfa4ef9dc3632756e5eb9d3349f109732e6e0b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-23 14:30:13 +0000

    0x13 - Task 6: Fix output of empty stack

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,12 +5,15 @@
  *               and removes it from the list.
  * @head: The list's head.
  *
- * Return: The old head's data (a number).
+ * Return: The old head's data (a number), or 0 if list is empty.
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *new_head;
 	int n;
+
+	if (*head == NULL)
+		return (0);
 
 	n = (*head)->n;
 
