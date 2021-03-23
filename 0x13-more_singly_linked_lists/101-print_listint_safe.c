5469cc51debacfab67f2f613e86ce604cc4775d1
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-23 20:07:34 +0000

    0x13 - Task 101: Solution redesign

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,82 @@
 #include "lists.h"
 
 /**
+ * add_node_endadd - Adds an address node at the end of a singly linked list.
+ * @head: The list's head.
+ * @add: The new node's address.
+ *
+ * Return: The address of the new element, or NULL if failed.
+ */
+listadd_t *add_node_endadd(listadd_t **head, void *add)
+{
+	listadd_t *new_node, *last_node;
+
+	last_node = *head;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->add = add;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	last_node->next = new_node;
+
+	return (new_node);
+}
+
+/**
+ * find_nodeadd - Finds if a value is already existent in the singly-linked
+ *                list.
+ * @head: The list's head.
+ * @add: The address to be found.
+ *
+ * Return: 1 if found, 0 otherwise.
+ */
+short find_nodeadd(listadd_t **head, void *add)
+{
+	listadd_t *current_node;
+
+	current_node = *head;
+	while (current_node != NULL)
+	{
+		if ((current_node)->add == add)
+			return (1);
+		current_node = current_node->next;
+	}
+	return (0);
+}
+
+/**
+ * free_listadd - Frees the singly-linked list.
+ * @head: The list's head.
+ */
+void free_listadd(listadd_t **head)
+{
+	listadd_t *next_node;
+
+	if (head == NULL)
+		return;
+
+	next_node = (*head)->next;
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+	*head = NULL;
+}
+
+/**
  * print_listint_safe - Prints singly linked lists and circular
  *                      singly linked lists.
  * @head: The list's head.
@@ -9,24 +85,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	listadd_t *head_add;
 	size_t list_size;
 	short flag;
 
 	list_size = flag = 0;
-	if (head == NULL)
-		exit(98);
+	head_add = NULL;
 
 	while (head != NULL && flag == 0)
 	{
+		add_node_endadd(&head_add, (void *) head);
 		printf("[%p] %d\n", (void *) head, head->n);
-		if (head <= head->next)
+		if (find_nodeadd(&head_add, (void *) head->next) == 1)
 		{
-			printf("-> [%p] %d\n", (void *) head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *) head, head->next->n);
 			flag = 1;
 		}
-		list_size++;
 		head = head->next;
+		list_size++;
 	}
 
+	free_listadd(&head_add);
 	return (list_size);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listadd_s - singly linked list with addresses
+ * @add: address
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for task 101.
+ */
+typedef struct listadd_s
+{
+	void *add;
+	struct listadd_s *next;
+} listadd_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
