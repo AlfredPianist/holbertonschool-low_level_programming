1a991850032b7e0b38455f38448c420df66acda2
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-24 00:56:13 +0000

    0x13 - Task 103: implement of Floyd's cycle detection algorithm

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,31 +1,6 @@
 #include "lists.h"
 
 /**
- * add_node_endadd - Adds an address node at the end of a singly linked list.
- * @head: The list's head.
- * @add: The new node's address.
- *
- * Return: The address of the new element, or NULL if failed.
- */
-listadd_t *add_node_endadd(listadd_t **head, void *add);
-
-/**
- * find_nodeadd - Finds if a value is already existent in the singly-linked
- *                list.
- * @head: The list's head.
- * @add: The address to be found.
- *
- * Return: 1 if found, 0 otherwise.
- */
-short find_nodeadd(listadd_t **head, void *add);
-
-/**
- * free_listadd - Frees the singly-linked list.
- * @head: The list's head.
- */
-void free_listadd(listadd_t **head);
-
-/**
  * find_listint_loop - Returns the address of the node when the loop starts
  *                     on a circular singly linked list, NULL if head doesn't
  *                     exist or not a circular singly linked list.
@@ -36,24 +11,31 @@ void free_listadd(listadd_t **head);
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listadd_t *head_add;
-
-	head_add = NULL;
+	listint_t *uro, *boros;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
-	while (head != NULL)
+	uro = head;
+	boros = head;
+
+	while (1)
 	{
-		add_node_endadd(&head_add, (void *) head);
-		if (find_nodeadd(&head_add, (void *) head->next) == 1)
-		{
-			free_listadd(&head_add);
-			return (head->next);
-		}
-		head = head->next;
+		if (uro == NULL || uro->next == NULL)
+			return (NULL);
+		uro = uro->next->next;
+		boros = boros->next;
+		if (uro == boros)
+			break;
 	}
 
-	free_listadd(&head_add);
-	return (NULL);
+	boros = head;
+
+	while (uro != boros)
+	{
+		uro = uro->next;
+		boros = boros->next;
+	}
+
+	return (boros);
 }
