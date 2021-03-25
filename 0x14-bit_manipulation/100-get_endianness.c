7116bc75c3f8fc813a2b888c3a0f04d24efb3b76
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-25 21:53:11 +0000

    0x14 - Fix task 100: betty parenthesis

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 
 /**
- * flip_bits - Counts the number of bits flipped to convert from one number
- *             to the other.
- * @n: The first number.
- * @m: The second number.
+ * get_endianness - Gets if the computer is little-endian or big-endian
  *
- * Return: The number of bits flipped.
+ * Description: Knowing that little-endian means the most significant byte
+ *              _MSB_ is stored last and big-endian means the opposite
+ *              _MSB first_, then I know that checking if the first byte of
+ *              the address in which an unsigned integer 1 is stored, if it
+ *              is 1, the MSB is last, meaning big-endian. 0 otherwise.
+ * Return: 0 if little-endian, 1 if big-endian.
  */
 int get_endianness(void)
 {
