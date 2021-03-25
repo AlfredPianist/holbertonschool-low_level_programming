443ebef27b7b36de8b944c219b61e9e61bf3d22f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-25 18:12:25 +0000

    0x14 - Fix task 2: betty and condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,13 +2,16 @@
 
 /**
  * get_bit - Returns the bit of a number given an index.
- * @b: The decimal number to be searched for the bit.
+ * @n: The decimal number to be searched for the bit.
  * @index: The index of the bit.
  *
  * Return: The bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
+
 	if ((n & (1 << index)) == 0)
 		return (0);
 	else
