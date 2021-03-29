d425370941db1255dbd79de7c196ede3e28cfcbd
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-03-29 19:49:17 +0000

    0x15: Add task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - Appends text_content to an existing file.
+ * @filename: Name of the file.
+ * @text_content: The content to be written to the file.
+ *
+ * Return: 1 if success. -1 if non-existent file or filename, couldn't open or
+ *         write the contents to the file.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	size_t buff_size;
+
+	file_descriptor = buff_size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[buff_size] != '\0')
+			buff_size++;
+
+		if (write(file_descriptor, text_content, buff_size) == -1)
+			return (-1);
+	}
+	close(file_descriptor);
+
+	return (1);
+}
