958c5566d5ce3f2324db9096233a668f45eca791
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-03 19:59:52 +0000

    Add: task 100 from 0x18

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,59 @@
+/**
+ * add - Sums to numbers.
+ * @a: First term.
+ * @b: Second term.
+ *
+ * Result: a + b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts to numbers.
+ * @a: First term.
+ * @b: Second term.
+ *
+ * Result: a - b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies to numbers.
+ * @a: First factor.
+ * @b: Second factor.
+ *
+ * Result: a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides to numbers.
+ * @a: Dividend.
+ * @b: Divisor.
+ *
+ * Result: a / b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - The modulo of two numbers.
+ * @a: Dividend.
+ * @b: Divisor.
+ *
+ * Result: a % b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
